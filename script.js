80e2afb8b4475f487a66d3ad5436f0c86f40900b
Jose Marcelino San Juan Catacutan <emp2011@coi.local>
2023-04-12 15:42:08 +0800

    added automatic close after cropping

--- script.js
+++ script.js
@@ -1,54 +1,73 @@
 $(function () {
-    const inputImage = document.getElementById("inputImage");
-    const cropButton = document.getElementById("cropButton");
-    const image = document.getElementById("image");
-  
-    let cropper;
-  
-    inputImage.addEventListener("change", (e) => {
-      const file = e.target.files[0];
-  
-      if (file) {
-        const reader = new FileReader();
-        reader.onload = (e) => {
-          image.src = e.target.result;
-          image.style.display = "block";
-  
+  const inputImage = document.getElementById("inputImage");
+  const cropButton = document.getElementById("cropButton");
+  const image = document.getElementById("image");
+  const imageCropperModal = $("#imageCropperModal");
+
+  let cropper;
+
+  inputImage.addEventListener("change", (e) => {
+    const file = e.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        image.src = e.target.result;
+        image.style.display = "block";
+
+        // Show the modal
+        imageCropperModal.modal("show");
+
+        // Adjust the image container dimensions
+        imageCropperModal.on("shown.bs.modal", () => {
+          const modalWidth = imageCropperModal.find(".modal-content").width();
+          const modalHeight = imageCropperModal.find(".modal-content").height();
+          const containerWidth = modalWidth * 0.9;
+          const containerHeight = modalHeight * 0.6;
+          image.parentElement.style.width = `${containerWidth}px`;
+          image.parentElement.style.height = `${containerHeight}px`;
+          image.parentElement.style.paddingBottom = "0";
+
           if (cropper) {
             cropper.destroy();
           }
-  
+
           cropper = new Cropper(image, {
             aspectRatio: 1,
             viewMode: 1,
             minCropBoxWidth: 300, // Minimum width of the crop box in pixels
             minCropBoxHeight: 300, // Minimum height of the crop box in pixels
           });
-        };
-        reader.readAsDataURL(file);
-      }
-    });
-  
-    cropButton.addEventListener("click", () => {
-      if (cropper) {
-        const croppedImageDataURL = cropper.getCroppedCanvas({
-            width: 300, // Desired width of the output image in pixels
-            height: 300, // Desired height of the output image in pixels
-        }).toDataURL();
-        // Set the src of the image element to the data URL of the cropped image
-        image.src = croppedImageDataURL;
-
-        // Download the cropped image
-        const link = document.createElement("a");
-        link.href = croppedImageDataURL;
-        link.download = "cropped-image.png";
-        link.click();
-
-        // Destroy the cropper instance and remove any cropping UI
-        cropper.destroy();
-        cropper = null;
-      } else {
-        alert("Please upload an image and adjust the crop area before cropping.");
-      }
-    });
+        });
+      };
+      reader.readAsDataURL(file);
+    }
   });
+
+  cropButton.addEventListener("click", () => {
+    if (cropper) {
+      const croppedImageDataURL = cropper.getCroppedCanvas({
+        width: 300, // Desired width of the output image in pixels
+        height: 300, // Desired height of the output image in pixels
+      }).toDataURL();
+      
+      // Set the src of the image element to the data URL of the cropped image
+      image.src = croppedImageDataURL;
+
+      // Download the cropped image
+      const link = document.createElement("a");
+      link.href = croppedImageDataURL;
+      link.download = "cropped-image.png";
+      link.click();
+
+      // Destroy the cropper instance and remove any cropping UI
+      cropper.destroy();
+      cropper = null;
+
+      // Close the modal
+      imageCropperModal.modal("hide");
+    } else {
+      alert("Please upload an image and adjust the crop area before cropping.");
+    }
+  });
+});
